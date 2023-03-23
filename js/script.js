797b4dbcae46d07fb67bf05cc5b34b47b7ddfd05
Spongey21 <driton.latifi101@gmail.com>
2023-03-23 13:51:19 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,60 +1,60 @@
-// const ANIMALS = document.querySelector(".animals");
-// const FOOD = document.querySelector(".food");
-// const POINT = document.querySelector("#point");
-// const SCORE = document.querySelector('#score');
+const ANIMALS = document.querySelector(".animals");
+const FOOD = document.querySelector(".food");
+const POINT = document.querySelector("#point");
+const SCORE = document.querySelector('#score');
 
-// const obj = [
-//   { animal: '🐵', food: '🍎', sound: './Sounds/Monkey.mp3' },
-//   { animal: '🦍', food: '🍌', sound: './Sounds/Gorilla.mp3' },
-//   { animal: '🐯', food: '🍍', sound: './Sounds/Tiger.mp3' }
-// ];
+const obj = [
+  { animal: '🐵', food: '🍎', sound: './Sounds/Monkey.mp3' },
+  { animal: '🦍', food: '🍌', sound: './Sounds/Gorilla.mp3' },
+  { animal: '🐯', food: '🍍', sound: './Sounds/Tiger.mp3' }
+];
 
-// SCORE.textContent = `Score: ${localStorage.getItem('score')}` 
+SCORE.textContent = `Score: ${localStorage.getItem('score')}` 
 
-// const pointSystem = function (target, event) {
-//   const animal = obj.find(function (animalObj) {
-//     return animalObj.animal == event.target.textContent;
-//   })
+const pointSystem = function (target, event) {
+  const animal = obj.find(function (animalObj) {
+    return animalObj.animal == event.target.textContent;
+  })
 
-//   if (animal.food == target.textContent) {
-//     POINT.dataset.value = parseInt(POINT.dataset.value) + 100;
-//     localStorage.setItem('score', POINT.dataset.value)
+  if (animal.food == target.textContent) {
+    POINT.dataset.value = parseInt(POINT.dataset.value) + 100;
+    localStorage.setItem('score', POINT.dataset.value)
 
-//     event.toElement.classList.add('animate__animated', 'animate__tada')
-//     const audio = new Audio(animal.sound);
-//     audio.play();
+    event.toElement.classList.add('animate__animated', 'animate__tada')
+    const audio = new Audio(animal.sound);
+    audio.play();
 
-//     setTimeout(function () {
-//       audio.pause();
-//     }, 4000);
-//   } else {
-//     POINT.dataset.value = parseInt(POINT.dataset.value) - 100;
-//   }
+    setTimeout(function () {
+      audio.pause();
+    }, 4000);
+  } else {
+    POINT.dataset.value = parseInt(POINT.dataset.value) - 100;
+  }
 
-//   POINT.textContent = `Point: ${POINT.dataset.value}`;
+  POINT.textContent = `Point: ${POINT.dataset.value}`;
   
-//   if (parseInt(POINT.dataset.value) <= 0) {
-//     alert('You failed!');
+  if (parseInt(POINT.dataset.value) <= 0) {
+    alert('You failed!');
 
-//     POINT.dataset.value = 0;
-//   }
-// };
+    POINT.dataset.value = 0;
+  }
+};
 
-// let dragged = null;
+let dragged = null;
 
-// for (const food of FOOD.children) {
-//   food.addEventListener("dragstart", function (event) {
-//     dragged = event.target;
-//   });
-// }
+for (const food of FOOD.children) {
+  food.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+  });
+}
 
-// for (const animal of ANIMALS.children) {
-//   animal.addEventListener("dragover", function (event) {
-//     event.preventDefault();
-//   });
+for (const animal of ANIMALS.children) {
+  animal.addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
 
-//   animal.addEventListener("drop", function (event) {
-//     event.preventDefault();
-//     pointSystem(dragged, event);
-//   });
-// }
+  animal.addEventListener("drop", function (event) {
+    event.preventDefault();
+    pointSystem(dragged, event);
+  });
+}
